2ec163521c96b79656831089ade757f5099a09ae
Siddhant Bansal <siddhant2697@gmail.com>
2018-02-12 20:53:20 +0530

    Create varaverb.py

---
+++ Week1/varaverb.py
@@ -0,0 +1,13 @@
+sid='Siddhant'
+num=10
+if type(varA) and type(varB)==type(num):
+    if varA>varB:
+        print('bigger')
+    elif varA==varB:
+        print('equal')
+    else :
+        print('smaller')
+elif type(varA) or type(varB)== type(sid):
+    print('string involved')
+
+

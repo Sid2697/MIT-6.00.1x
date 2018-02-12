f7dce61cf12e0c260c27dd88aa2348518b98703d
Siddhant Bansal <siddhant2697@gmail.com>
2018-02-12 21:04:57 +0530

    Create powerrecur.py

---
+++ Week2/powerrecur.py
@@ -0,0 +1,15 @@
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    
+    if exp==1:
+        return base
+    elif exp==0:
+        return 1
+    else:
+        return base*recurPower(base,exp-1)
